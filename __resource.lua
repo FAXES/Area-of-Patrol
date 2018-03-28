9d9cde2e3d38a4181916b62659fa3d04c5c8b406
FAXES <31368070+FAXES@users.noreply.github.com>
2018-03-28 18:23:01 +1100

    Added Permissions

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,4 @@
--- AOP by FAXES
+-- Area of Patrol by FAXES
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_script "client.lua"
--- client.lua
+++ client.lua
@@ -1,14 +1,11 @@
 -- Made by FAXES
-RegisterNetEvent('AOPList')
-AddEventHandler('AOPList', function()
-	TriggerEvent("chatMessage", "AOP Script", {255, 0, 0}, "^6Blaine County [/aopbc], ^8State Wide [/aopstate], ^6Los Santos [/aopls], ^8Vinewood [/aopvw], ^6Pillbox Hill / Downtown [aopdt], ^8Davis [/aopdavis], ^6Del Perro [/aopdp], ^8Rockford Hills [/aoprh], ^6Sandy Shores [/aopsandy], ^8Harmony [/aopharm], ^6GrapeSeed [/aopgrape], ^8Paleto Bay [/aoppb], ^4Peace Time On [/pton], Peace Time Off [/ptoff], ^3RP Area Vote [/aopvote]")
---		^->^ Edit the list if you wish            ^This sets the color                            ^List
+RegisterNetEvent('AOP:List')
+AddEventHandler('AOP:List', function()
+	TriggerEvent("chatMessage", "AOP Script", {255, 0, 0}, "Blaine County [/aopbc], State Wide [/aopstate], Los Santos [/aopls], Vinewood [/aopvw], Mirror Park [/aopmp] Pillbox Hill / Downtown [aopdt], Davis [/aopdavis], Del Perro [/aopdp], Rockford Hills [/aoprh], Sandy Shores [/aopsandy], Harmony [/aopharm], GrapeSeed [/aopgrape], Paleto Bay [/aoppb], ^4Peace Time On [/pton], Peace Time Off [/ptoff], ^3RP Area Vote [/aopvote]")
 	end)
---
--- This code has been left here for a further date. Don't un comment it as the code is wrong and won't work.
---
---RegisterNetEvent('AOPBC')
---AddEventHandler('AOPBC', function()
---	TriggerEvent("chatMessage", -1, " \n —————————————————————— \n RP AREA IS NOW : BLAINE COUNTY \n Please Finish Your Current RP and Move. \n ——————————————————————", {239, 0, 0}, " ")
---	TriggerClientEvent("chat:addMessage", -1, { args = { "", "Test Message"} })
---end)
+	
+RegisterNetEvent('AOP:NoPerms')
+AddEventHandler('AOP:NoPerms', function()
+	TriggerEvent("chatMessage", "AOP Script", {255, 0, 0}, "Insignificant Permissions to run command.")
+	end)
+-- Made by FAXES
